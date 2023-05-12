7347e624654f345137042bb96bc7e64f0515498f
Olakunleabiola <132951450+Olakunleabiola@users.noreply.github.com>
2023-05-11 18:21:06 -0700

    Add Python file
    This python file demonstrates how to add python code to GitHub.

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_person_info(name, age, weight, height):
+    print("Name:", name)
+    print("Age:", age)
+    print("Weight:", weight)
+    print("Height:", height//12, "ft", height%12, "inches")
+    print()
+
+print_person_info("Joe", 22, 123, 69)
+print_person_info("Josh", 32, 156, 74)
+print_person_info("Mabel", 21, 106, 62)
